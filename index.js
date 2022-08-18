1593e301af1b80b1149c8aae347912a339d40376
frednguyen123 <fred.nguyen.2000@gmail.com>
2022-08-18 01:50:16 -0700

    Completed Movie Library

--- index.js
+++ index.js
@@ -1,19 +1,45 @@
 // API: http://www.omdbapi.com/?i=tt3896198&apikey=1b90d60f
 // API SEARCH: https://www.omdbapi.com/?apikey=1b90d60f&s=fast
 
+const movieListElement = document.querySelector('.movies__wrapper');
+const movieNameInput = document.querySelector('input').value
 
-async function renderMovies(movieName){
-    let searchedMovie = movieName.target.value;
-    // console.log(searchedMovie);
-    const movies = await fetch(`https://www.omdbapi.com/?apikey=1b90d60f&s=${searchedMovie}`);
-    const moviesData = await movies.json();
-    console.log(moviesData);
-    const mappedArray = moviesData.map((movie) => movieHTML(movie))
-    .join("")
-
-    console.log(mappedArray);
+async function buttonSearch(){
+    const movieNameEnterButton = document.querySelector('input').value;
+    console.log(movieNameEnterButton);
+    renderMovies(movieNameEnterButton);
 }
 
-// function movieHTML(movie){
-//     return ``
-// }
+async function search(event){
+    const movieNameEnterKey = document.querySelector('input').value
+    console.log(movieNameEnterKey);
+    if (event.keyCode === 13) {
+        renderMovies(movieNameEnterKey);
+    }
+}
+
+async function renderMovies(movieName){
+    /**
+     * ONLY USE THE COMMENTED OUT CODE IF YOU USE "onchange="renderMovies(event)" ON THE INPUT CLASS .header__search-bar--input
+     * This will allow you to target the value when it is changed, works when you submit form
+     */
+    // let searchedMovie = movieName.target.value;
+    // console.log(searchedMovie);
+    const movies = await fetch(`https://www.omdbapi.com/?apikey=1b90d60f&s=${movieName}`);
+    const moviesData = await movies.json();
+    console.log(moviesData);
+    console.log(moviesData.Search);
+    movieListElement.innerHTML = (moviesData.Search.slice(0, 6)).map((movie) => movieHTML(movie))
+    .join("");
+}
+
+function movieHTML(movie){
+    return `<div class="movie">
+                <figure class="movie__img--wrapper">
+                    <img class="movie__img" src="${movie.Poster}" alt="">
+                </figure>
+                <div class="movie__title">
+                    ${movie.Title} (${movie.Year})
+                </div>
+            </div>`;
+}
